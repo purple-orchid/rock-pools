6f7624b6b8db372dc9d3d1a8e62351821201a2ed
Rose Duke <94328321+purple-orchid@users.noreply.github.com>
2023-03-07 18:08:48 +1300

    Added inputs to form and handleChange function.

--- src/pages/form.js
+++ src/pages/form.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 export default function Form(){
   const [state, setState] = useState ({
-    date : "",
     title : "",
+    date : "",
     description : "",
-    image : "",
+    photo : "",
     locationDescription : "",
     locationImage : "",
     gpsCoordinates : "",
@@ -16,10 +16,30 @@ export default function Form(){
     alert(state);
   }
 
+  function handleChange (e) {
+    const name =  e.target.name;
+    const value = e.target.value;
+    setState ({...state, [name] : value});
+  }
+
   return (
     <form onSubmit={handleSubmit} >
-
-
+      <label for="title">Title: </label>
+      <input type="text" id="title" name="title" value={state.title} onChange={handleChange}/>
+      <label for="date">Date: </label>
+      <input type="date" id="date" name="date" value={state.date} onChange={handleChange}/>
+      <label for="description">Description: </label>
+      <textarea id="description" name="description" rows="2" cols="70" value={state.description} onChange={handleChange}/>
+      <label for="photo">Upload a photo: </label>
+      <input type="file" id="photo" name="photo" value={state.photo} onChange={handleChange}/>
+      <label for="locationDescription">Location description: </label>
+      <textarea id="locationDescription" name="locationDescription" rows="2" cols="70" value={state.locationDescription} onChange={handleChange}/>
+      <label for="locationPhoto">Upload a photo of the location: </label>
+      <input type="file" id="locationPhoto" name="locationPhoto" value={state.locationPhoto} onChange={handleChange}/>
+      <input type="checkbox" id="gpsConfirm" name="gpsConfirm" value="Yes"/>
+      <label for="gpsConfirm"> Allow GPS coordinates of device to be recorded</label>
+      // Need to figure out how to get gps coordinates from user.
+      <input type="hidden" id="gpsCoordinates" name="gpsCoordinates" value={state.gpsCoordinates}/>
     </form>
   )
 }
